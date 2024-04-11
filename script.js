f59537c0d2c067e82cdccff58851af3518fc3cca
TheAdeyemiOlayinka <olayinkaapps@gmail.com>
2024-04-11 17:18:02 +0100

    feat: scrollreveal

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 // Script for navigation bar
+
+// Script for Scroll Reveal
+ScrollReveal().reveal(".features", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
+ScrollReveal().reveal("#featured-products", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
+ScrollReveal().reveal(".product-card", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
+ScrollReveal().reveal("#services", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
+ScrollReveal().reveal(".blog-post", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
+ScrollReveal().reveal("#newsletter", {
+  delay: 300,
+  distance: "50px",
+  origin: "bottom",
+  easing: "ease-in-out",
+  duration: 800,
+});
